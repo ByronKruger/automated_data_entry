e5f3bed83e2c3be1a80d80ae9d7b1d016967cd48
ByronKruger <byronkruger3@gmail.com>
2019-08-24 23:16:41 +0200

    added stride capabilty with sliding window

--- window_slider.py
+++ window_slider.py
@@ -37,9 +37,11 @@ def slide_windows(filename, anchor_box, stride=1):
 	# this prevents out of bound indexes when snipping out windows of the image array
 	
 	# hori_window_steps = (image_wid // anchor_box[0]) * stride
-	hori_window_steps = image_wid // anchor_box[0] 
+	# hori_window_steps = (image_wid // anchor_box[0]) * 2
+	hori_window_steps = (image_wid // anchor_box[0]) * 2
 	# vert_window_steps = (image_hei // anchor_box[1]) * stride
-	vert_window_steps = image_hei // anchor_box[1]
+	# vert_window_steps = (image_hei // anchor_box[1]) * 2
+	vert_window_steps = (image_hei // anchor_box[1]) * 2
 
 	poss_objects = cv2.imread(filename)
 	poss_objects = cv2.cvtColor(poss_objects, cv2.COLOR_BGR2RGB)
@@ -48,8 +50,12 @@ def slide_windows(filename, anchor_box, stride=1):
 
 	for hori_step in range(hori_window_steps):
 		for vert_step in range(vert_window_steps):
-			poss_object = poss_objects[vert_step * anchor_box[1]: vert_step * anchor_box[1] + anchor_box[1],\
-									   hori_step * anchor_box[0]: hori_step * anchor_box[0] + anchor_box[0]]
+			print("y: " + str(int(vert_step * anchor_box[1] * 0.5)) + ":" + str(int(vert_step * anchor_box[1] * 0.5 + anchor_box[1] * 0.5)))
+			print("x: " + str(int(hori_step * anchor_box[0] * 0.5)) + ":" + str(int(hori_step * anchor_box[0] * 0.5 + anchor_box[0] * 0.5)))
+			poss_object = poss_objects[int(vert_step * anchor_box[1] * 0.5): int(vert_step * anchor_box[1] * 0.5 + anchor_box[1]),\
+									   int(hori_step * anchor_box[0] * 0.5): int(hori_step * anchor_box[0] * 0.5 + anchor_box[0])]
+			# poss_object = poss_objects[int(vert_step * anchor_box[1]): int(vert_step * anchor_box[1] + anchor_box[1]),\
+			# 						   int(hori_step * anchor_box[0]): int(hori_step * anchor_box[0] + anchor_box[0])]
 
 			img = Image.fromarray(poss_object)
 
@@ -83,11 +89,11 @@ def slide_windows(filename, anchor_box, stride=1):
 	return hog_windows
 
 if __name__ == "__main__":
-	filename = "/home/charlie/Desktop/yolo/yolo-v3/data/coco/cocoapi/coco/images/train2017/train2017/000000001756.jpg"
+	filename = "/home/charlie/Desktop/yolo/yolo-v3/data/coco/cocoapi/coco/images/train2017/train2017/000000000715.jpg"
 	anchor_box = []
 	anchor_box.append(50) # w
-	anchor_box.append(100) # h
+	anchor_box.append(50) # h
 
 	hog_windows = slide_windows(filename, anchor_box, 2)
 
-	train_and_predict_multiclass(['orange', 'bicycle', 'motorbike', 'person'], '200', '7', hog_windows)
+	train_and_predict_multiclass(['orange', 'tvmonitor', 'tennis racket', 'person'], '200', '7', hog_windows)
